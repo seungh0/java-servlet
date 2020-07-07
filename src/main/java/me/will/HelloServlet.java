57ccb1cd5de618ec0b5a9c44032ce09d41f1a232
seungh0 <will.seungho@gmail.com>
2020-07-08 07:34:21 +0900

    fix error

--- src/main/java/me/will/HelloServlet.java
+++ src/main/java/me/will/HelloServlet.java
@@ -18,10 +18,10 @@ public class HelloServlet extends HttpServlet {
 		System.out.println("doGet()");
 		resp.getWriter().println("<html>");
 		resp.getWriter().println("<head>");
+		resp.getWriter().println("</head>");
 		resp.getWriter().println("<body>");
 		resp.getWriter().println("<h1>Hello Servlet</h1>");
 		resp.getWriter().println("</body>");
-		resp.getWriter().println("</head>");
 		resp.getWriter().println("</html>");
 	}
 
