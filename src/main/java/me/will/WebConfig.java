c2cbcae46db1d7433f29fa4c501be83aae9eb28b
seungh0 <will.seungho@gmail.com>
2020-07-08 11:38:28 +0900

    add no hierarcy strucutre.

--- src/main/java/me/will/WebConfig.java
+++ src/main/java/me/will/WebConfig.java
@@ -2,7 +2,6 @@ package me.will;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
 /**
  * DispatcherServlet - Root WebApplicationContext
@@ -11,6 +10,6 @@ import org.springframework.stereotype.Controller;
  * https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc
  */
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan
 public class WebConfig {
 }
