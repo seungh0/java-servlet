ac55a3dd4472de43a7688360b78c0398c4824635
seungh0 <will.seungho@gmail.com>
2020-07-08 11:29:33 +0900

    add dispatcherservlet document.

--- src/main/java/me/will/AppConfig.java
+++ src/main/java/me/will/AppConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
+/**
+ * DispatcherServlet - Servlet WebApplicationContext
+ * (containing controllers, view resolvers, and other web-related beans)
+ *
+ * https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc
+ */
 @Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
 public class AppConfig {
