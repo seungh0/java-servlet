3500b7950cdf2a5113a0fdd5b35e1e30fef92d41
seungh0 <will.seungho@gmail.com>
2020-07-09 08:43:47 +0900

    DispatcherServlet - add ViewResolver Bean

--- src/main/java/me/will/WebConfig.java
+++ src/main/java/me/will/WebConfig.java
@@ -1,7 +1,10 @@
 package me.will;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * DispatcherServlet - Root WebApplicationContext
@@ -12,4 +15,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class WebConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
 }
